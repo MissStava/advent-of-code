7602213bcc672bf85f7158330beed9b0535aaf3c
Stu <null.method@gmail.com>
2017-04-26 14:23:34 +0100

    Correct implementation of python day 3 part 2

--- 2016/python/day3/day3pt2.py
+++ 2016/python/day3/day3pt2.py
@@ -1,16 +1,18 @@
-file = open("day3.txt", "r")
+file = open("../inputs/day3.txt", "r")
 
 validTriangles = 0
 
 elements = [int(x) for x in file.read().replace('\n', ' ').split()]
 
-
-for line in file:
-	side1, side2, side3 = [int(x) for x in line.split()]
-
-	if side1 + side2 > side3 and side2 + side3 > side1 and side3 + side1 > side2:
-		validTriangles += 1
+for x in range(0,len(elements),9):
+	for y in range(3):
+		if elements[x+y+0] + elements[x+y+3] > elements[x+y+6] and elements[x+y+3] + elements[x+y+6] > elements[x+y+0] and elements[x+y+6] + elements[x+y+0] > elements[x+y+3] :
+			validTriangles += 1
 
 print validTriangles
 
-# Part1 = 1032
+# 1332 (too low)
+# 1714 (too low)
+# 1914 (too high)
+# 1889 (not right)
+# Part 2 answer is 1839
