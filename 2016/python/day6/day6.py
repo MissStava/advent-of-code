c3b6eaab04ec6fbd2d3c0c0bcac31820e6436c54
Stu <null.method@gmail.com>
2017-05-02 13:28:25 +0100

    Python day 6 part 2

--- 2016/python/day6/day6.py
+++ 2016/python/day6/day6.py
@@ -4,8 +4,17 @@ file = open("../inputs/day6.txt", "r")
 
 words = file.read().split('\n')
 
+password = ""
 for x in range(8):
 	letters = [word[x] for word in words]
-	print collections.Counter(letters).most_common(1)[0][0]
+	password += collections.Counter(letters).most_common(1)[0][0]
+print password
+
+password = ""
+for x in range(8):
+	letters = [word[x] for word in words]
+	password += collections.Counter(letters).most_common()[-1][0]
+print password
 
 # xhnqpqql
+# brhailro
