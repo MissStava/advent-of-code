99409a48934abbaa876de452a037573690187ab5
nullMethod <null.method@gmail.com>
2017-05-20 23:59:04 +0100

    Further attempt at day11

--- 2016/python/day11/day11.py
+++ 2016/python/day11/day11.py
@@ -2,11 +2,16 @@ import copy
 from itertools import combinations
 
 goalState = [[],[],[],["HG","HM","LG","LM"]]
-seenStates = []
+seenStates = [[["HM", "LM"],["HG"],["LG"],[]]]
 statesToTry = [([["HM", "LM"],["HG"],["LG"],[]],0,0)]
+# statesToTry = [([[], ['HM'], ['HG', 'LG', 'LM'], []], 1, 0)]
 
 def validateFloor(floor):
 	valid = True
+
+	if floor == []:
+		return valid
+
 	for item in floor:
 		element = item[0]
 		component = item[1]
@@ -14,12 +19,14 @@ def validateFloor(floor):
 			valid = False
 	return valid
 
+
+
 goalStateFound = False
 goalSteps = 0
 while not goalStateFound:
-#for j in range(10):
+# for j in range(4):
 
-	print statesToTry
+	print "statesToTry = " + str(statesToTry)
 
 	nextStateToTry = statesToTry.pop(0)
 	state = nextStateToTry[0]
@@ -28,49 +35,12 @@ while not goalStateFound:
 	elevatorUp = elevator + 1
 	elevatorDown = elevator - 1
 
-	print state
-
-	if elevator < 3:
-		currentState = copy.deepcopy(state)
-		floor = currentState[elevator]
-		floorUp = currentState[elevatorUp]
-
-		for item in floor:
-
-			xCurrentState = copy.deepcopy(currentState)
-			xFloor = xCurrentState[elevator]
-			xFloorUp = xCurrentState[elevatorUp]
-
-			xFloorUp.append(item)
-			xFloorUp = sorted(xFloorUp)
-			xFloor.remove(item)
-
-			xCurrentState[elevator] = xFloor
-			xCurrentState[elevatorUp] = xFloorUp
-
-			for x in range(4):
-				if xCurrentState[x] == None:
-					xCurrentState[x] = []
-
-			if xCurrentState == goalState:
-				goalStateFound = True
-				goalSteps = steps
-				print "SUCCESS"
-				break
-
-
-			if xCurrentState in seenStates:
-				print "state seen"
-				continue
-			else:
-				seenStates.append((xCurrentState))
-
-			if validateFloor(xFloorUp):
-				statesToTry.append((xCurrentState, elevatorUp, steps))
+	if elevator < 3 and not goalStateFound:
 
 		currentState = copy.deepcopy(state)
 		floor = currentState[elevator]
 		floorUp = currentState[elevatorUp]
+		multiItemMoved = False
 
 		for items in combinations(floor, 2):
 
@@ -90,55 +60,73 @@ while not goalStateFound:
 				if xCurrentState[x] == None:
 					xCurrentState[x] = []
 
-			if xCurrentState == goalState:
-				goalStateFound = True
-				goalSteps = steps
-				print "SUCCESS"
-				break
-
-			if xCurrentState in seenStates:
-				print "state seen"
+			if (items, xFloorUp) in seenStates:
 				continue
 			else:
-				seenStates.append(xCurrentState)
+				if validateFloor(xFloorUp) and validateFloor(xFloor):
+					if xCurrentState == goalState:
+						goalStateFound = True
+						goalSteps = steps
+						print "SUCCESS"
+						break
 
-			if validateFloor(xFloorUp):
-				statesToTry.append((xCurrentState, elevatorUp, steps))
+					print "xCurrentState = " + str(xCurrentState)
+					print "goalState = " + str(goalState)
+					print "steps = " + str(steps)
+					print
+					statesToTry.append((xCurrentState, elevatorUp, steps))
+					seenStates.append((items, xFloorUp))
+					multiItemMoved = True
 
-	if elevator > 0:
+		if goalStateFound:
+			break
+
+		if not multiItemMoved:
+			currentState = copy.deepcopy(state)
+			floor = currentState[elevator]
+			floorUp = currentState[elevatorUp]
+
+			for item in floor:
+
+				xCurrentState = copy.deepcopy(currentState)
+				xFloor = xCurrentState[elevator]
+				xFloorUp = xCurrentState[elevatorUp]
+
+				xFloorUp.append(item)
+				xFloorUp = sorted(xFloorUp)
+				xFloor.remove(item)
+
+				xCurrentState[elevator] = xFloor
+				xCurrentState[elevatorUp] = xFloorUp
+
+				for x in range(4):
+					if xCurrentState[x] == None:
+						xCurrentState[x] = []
+
+				if (item, xFloorUp) in seenStates:
+					continue
+				else:
+					if validateFloor(xFloorUp) and validateFloor(xFloor):
+						if xCurrentState == goalState:
+							goalStateFound = True
+							goalSteps = steps
+							print "SUCCESS"
+							break
+
+						print "xCurrentState = " + str(xCurrentState)
+						print "goalState = " + str(goalState)
+						print "steps = " + str(steps)
+						print
+
+						statesToTry.append((xCurrentState, elevatorUp, steps))
+						seenStates.append((item, xFloorUp))
+
+	if elevator > 0 and not goalStateFound:
+
 		currentState = copy.deepcopy(state)
 		floor = currentState[elevator]
 		floorDown = currentState[elevatorDown]
-
-		for item in floor:
-
-			xCurrentState = copy.deepcopy(currentState)
-			xFloor = xCurrentState[elevator]
-			xFloorDown = xCurrentState[elevatorDown]
-
-			xFloorDown.append(item)
-			xFloorDown = sorted(xFloorDown)
-			xFloor.remove(item)
-
-			xCurrentState[elevator] = xFloor
-			xCurrentState[elevatorDown] = xFloorDown
-
-			for x in range(4):
-				if xCurrentState[x] == None:
-					xCurrentState[x] = []
-
-			if xCurrentState in seenStates:
-				print "state seen"
-				continue
-			else:
-				seenStates.append(xCurrentState)
-
-			if validateFloor(xFloorDown):
-				statesToTry.append((xCurrentState, elevatorDown, steps))
-
-		currentState = copy.deepcopy(state)
-		floor = currentState[elevator]
-		floorDown = currentState[elevatorDown]
+		multiItemMoved = False
 
 		for items in combinations(floor, 2):
 
@@ -158,49 +146,56 @@ while not goalStateFound:
 				if xCurrentState[x] == None:
 					xCurrentState[x] = []
 
-			if xCurrentState in seenStates:
-				print "state seen"
+			if (item, floorDown) in seenStates:
 				continue
 			else:
-				seenStates.append(xCurrentState)
+				if validateFloor(xFloorDown) and validateFloor(xFloor):
+					if xCurrentState == goalState:
+						goalStateFound = True
+						goalSteps = steps
+						print "SUCCESS"
+						break
 
-			if validateFloor(xFloorDown):
-				statesToTry.append((xCurrentState, elevatorDown, steps))
+					statesToTry.append((xCurrentState, elevatorDown, steps))
+					seenStates.append((item, floorDown))
+					multiItemMoved = True
+		
+		if goalStateFound:
+			break
+
+		if not multiItemMoved:
+			currentState = copy.deepcopy(state)
+			floor = currentState[elevator]
+			floorDown = currentState[elevatorDown]
+
+			for item in floor:
+
+				xCurrentState = copy.deepcopy(currentState)
+				xFloor = xCurrentState[elevator]
+				xFloorDown = xCurrentState[elevatorDown]
+
+				xFloorDown.append(item)
+				xFloorDown = sorted(xFloorDown)
+				xFloor.remove(item)
+
+				xCurrentState[elevator] = xFloor
+				xCurrentState[elevatorDown] = xFloorDown
+
+				for x in range(4):
+					if xCurrentState[x] == None:
+						xCurrentState[x] = []
+
+				if (item, floorDown) in seenStates:
+					continue
+				else:
+					if validateFloor(xFloorDown) and validateFloor(xFloor):					
+						if xCurrentState == goalState:
+							goalStateFound = True
+							goalSteps = steps
+							print "SUCCESS"
+							break
+
+						statesToTry.append((xCurrentState, elevatorDown, steps))
+						seenStates.append((item, floorDown))		
 
 print "number of steps = " + str(goalSteps)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# get the first state in the queue
-#
-# get the items on the current floor of the current state
-#
-#	what are the possible states that can occur going up
-#	
-#		is the state the goal state
-#
-#		has the state been seen before
-#		
-#		is the state valid
-#
-#		add state to the queue
-#
-#	when no valid states added for going up
-#	
-#		what are the possible states that can occur going down
-#
-#			has the state been seen before
-#		
-#			is the state valid
-#
-#			add state to the queue
