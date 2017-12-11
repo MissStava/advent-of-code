1fe94e06013acd60350fc2f569d5ebf7438c7da9
Stu Hilton <stuart.hilton@travisperkins.co.uk>
2017-12-11 09:39:42 +0000

    2017 day 11 in python

---
+++ 2017/python/day11.py
@@ -0,0 +1,37 @@
+file = open("day11inputs", "r")
+input = file.read()
+steps = input.split(',')
+
+location = (0,0,0)
+maxDistance = 0
+
+for step in steps:
+    if step == 'n':
+        location = (location[0],location[1]+1,location[2]-1)
+
+    elif step == 'ne':
+        location = (location[0]+1,location[1],location[2]-1)
+
+    elif step == 'se':
+        location = (location[0]+1,location[1]-1,location[2])
+
+    elif step == 's':
+        location = (location[0],location[1]-1,location[2]+1)
+
+    elif step == 'sw':
+        location = (location[0]-1,location[1],location[2]+1)
+
+    elif step == 'nw':
+        location = (location[0]-1,location[1]+1,location[2])
+
+    tempLocation = [abs(n) for n in location]
+    tempMaxDistance = max(tempLocation)
+    if tempMaxDistance > maxDistance:
+        maxDistance = tempMaxDistance
+
+print location
+# 522 is too low
+# 765 is too high
+
+print maxDistance
+# 1532
