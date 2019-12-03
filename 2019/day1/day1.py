776842c4aac067e7d7d495481d5503fa7cf4874a
Stephanie Hilton <stephanie.hilton@travisperkins.co.uk>
2019-12-03 23:18:30 +0000

    Refactoring day 1

--- 2019/day1/day1.py
+++ 2019/day1/day1.py
@@ -1,23 +1,12 @@
-import math
-
 file = open("input", "r")
 
-masses = [int(x.strip('\n)')) for x in file.readlines()]
-
 def calculate_fuel(mass):
-    fuel = math.floor(mass/3)-2
+    fuel = mass // 3 - 2
     if fuel <= 0:
         return 0
     return calculate_fuel(fuel) + fuel
 
+print int(sum([calculate_fuel(int(mass)) for mass in file.readlines()]))
 
-total_fuel = 0
-for mass in masses:
-    total_fuel += calculate_fuel(mass)
-
-print int(total_fuel)
-
-
-# Day 1 - 3373568
-
- # 5057371
+# 3373568
+# 5057371
