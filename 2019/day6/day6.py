e3a1cf9855612eca6526e9cba7372ffde4855c3e
Stephanie Hilton <stephanie.hilton@travisperkins.co.uk>
2019-12-11 15:34:06 +0000

    WIP 2019 day 6

--- 2019/day6/day6.py
+++ 2019/day6/day6.py
@@ -3,11 +3,42 @@ lines = [line.strip('\n') for line in file.readlines()]
 
 orbits = {}
 for line in lines:
-    orbitee, orbiter = line.split(')')
-    if orbitee not in orbits:
-        orbits[orbitee] = []
-    if orbiter not in orbits:
-        orbits[orbiter] = []
-    orbits[orbitee].append(orbiter)
+    parent, child = line.split(')')
+    orbits[child] = parent
 
-print orbits
+def count_orbits(node, count):
+    if node == 'COM':
+        return count
+    count += 1
+    return count_orbits(orbits[node], count)
+
+def get_key(graph, val):
+    return [key for key, value in graph.items() if val == value]
+
+def bfs(graph, start, end):
+    queue = [[start]]
+    visited = []
+
+    while queue:
+        path = queue.pop(0)
+        node = path[-1]
+        if node not in visited:
+            neighbours = get_key[graph, node]
+
+            for neighbour in neighbours:
+                new_path = list(path)
+                new_path.append(neighbour)
+                queue.append(new_path)
+
+                if neighbour == end:
+                    return new_path
+
+            visited.append(node)
+    return None
+
+
+
+print sum([count_orbits(node, 0) for node in orbits])
+# 162816
+
+print bfs(orbits, 'YOU', 'SAN')
