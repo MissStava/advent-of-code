0bd3d9e0845cef6ccbdf103a6240f1d4aed90758
Stephanie Hilton <stephanie.hilton@travisperkins.co.uk>
2021-03-31 13:05:00 +0100

    Refactored 2015 day 1

---
+++ 2015/day1.py
@@ -0,0 +1,19 @@
+UP = '('
+DOWN = ')'
+
+with open('day1.input', 'r') as file:
+  data = file.read().replace('\n', '')
+
+final_floor = data.count(UP) - data.count(DOWN)
+print("Part 1 answer = {}".format(final_floor))
+
+current_floor = 0
+for count, value in enumerate(data, 1):
+  if value == UP:
+    current_floor += 1
+  else:
+    current_floor -= 1
+
+  if current_floor < 0:
+    print("Part 2 answer = {}".format(count))
+    break
