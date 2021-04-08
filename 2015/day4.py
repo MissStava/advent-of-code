dd7894cac24fddc225e90e3e54138a566bd2bb33
Stephanie Hilton <stephanie.hilton@travisperkins.co.uk>
2021-04-08 16:32:34 +0100

    2015 day 4 in python

---
+++ 2015/day4.py
@@ -0,0 +1,20 @@
+import hashlib
+
+def find_increment(input, final_state):
+  increment = 1
+
+  while True:
+    encoded_input = (input + str(increment)).encode("utf-8")
+    hashed_input = hashlib.md5(encoded_input).hexdigest()
+
+    if hashed_input[:len(final_state)] == final_state:
+      break
+    
+    increment += 1
+
+  return increment
+
+input = "yzbqklnj"
+
+print("Part 1 answer = {}".format(find_increment(input, "00000")))
+print("Part 2 answer = {}".format(find_increment(input, "000000")))
